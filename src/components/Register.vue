ca9984e389416f4a7b899a775a4a2e32296e28cc
Mario <mariodml90@gmail.com>
2019-07-05 18:55:20 -0500

    Added loggin and register users

---
+++ src/components/Register.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col s12 m8 offset-m2">
+          <div class="login card-panel grey lighten-4 black-text center">
+            <h3>Registro</h3>
+            <form>
+              <div class="input-field">
+                <i class="material-icons prefix">email</i>
+                <input type="text" id="email" v-model="email" />
+                <label for="email">Email</label>
+              </div>
+              <div class="input-field">
+                <i class="material-icons prefix">lock</i>
+                <input type="password" id="password" v-model="password" />
+                <label for="password">Contraseña</label>
+              </div>
+              <button v-on:click="register" class="btn btn-large grey lighten-4 black-text">Enviar</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+
+export default {
+  name: "register",
+  data: function() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    register: function(e) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(
+          user => {
+            alert(`Cuenta creada para ${user.user.email}`);
+            //this.$router.push('/');
+            this.$router.go({ path: this.$router.path });
+          },
+          err => {
+            alert(err.message);
+          }
+        );
+      e.preventDefault();
+    }
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,103 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Dashboard from '@/components/Dashboard'
-import NuevoEmpleado from '@/components/NuevoEmpleado'
-import EditarEmpleado from '@/components/EditarEmpleado'
-import VistaEmpleado from '@/components/VistaEmpleado'
+import Vue from "vue";
+import Router from "vue-router";
+import Dashboard from "@/components/Dashboard";
+import NuevoEmpleado from "@/components/NuevoEmpleado";
+import EditarEmpleado from "@/components/EditarEmpleado";
+import VistaEmpleado from "@/components/VistaEmpleado";
+import Login from "@/components/Login";
+import Register from "@/components/Register";
+import firebase from "firebase";
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
+let router = new Router({
   routes: [
     {
-      path: '/',
-      name: 'dashboard',
-      component: Dashboard
+      path: "/",
+      name: "dashboard",
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
-      path: '/new',
-      name: 'nuevo-empleado',
-      component: NuevoEmpleado
+      path: "/login",
+      name: "login",
+      component: Login,
+      meta: {
+        requiresGuest: true
+      }
     },
     {
-      path: '/edit/:empleado_id',
-      name: 'editar-empleado',
-      component: EditarEmpleado
+      path: "/register",
+      name: "register",
+      component: Register,
+      meta: {
+        requiresGuest: true
+      }
     },
     {
-      path: '/:empleado_id',
-      name: 'vista-empleado',
-      component: VistaEmpleado
+      path: "/new",
+      name: "nuevo-empleado",
+      component: NuevoEmpleado,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/edit/:empleado_id",
+      name: "editar-empleado",
+      component: EditarEmpleado,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/:empleado_id",
+      name: "vista-empleado",
+      component: VistaEmpleado,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
-})
+});
+
+//Nav Guards
+router.beforeEach((to, from, next) => {
+  //Check for requireAuth guard
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    //Check if NOT logged in
+    if (!firebase.auth().currentUser) {
+      // Go to login
+      next({
+        path: "/login",
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    } else {
+      // Proceed to route
+      next();
+    }
+  } else if (to.matched.some(record => record.meta.requiresGuest)) {
+    //Check if logged in
+    if (firebase.auth().currentUser) {
+      // Go to dashboard
+      next({
+        path: "/",
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    } else {
+      // Proceed to route
+      next();
+    }
+  } else {
+    // Proceed to route
+    next();
+  }
+});
+
+export default router;
